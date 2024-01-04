2300f159c1a02deaaed89b23622ebbaf388ccd5e
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-04 14:12:59 +0000

    task 2: spell check arguments

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -9,7 +9,7 @@ if __name__ == "__main__":
     elif arg_num == 2:
         print("{} argument:".format(arg_num - 1))
     else:
-        print("{} argumanets:".format(arg_num - 1))
+        print("{} arguments:".format(arg_num - 1))
 
     for x in range(1, arg_num):
         print("{}: {}".format(x, argv[x]))
