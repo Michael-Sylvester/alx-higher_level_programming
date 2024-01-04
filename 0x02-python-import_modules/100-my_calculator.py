d21bba2e2a6d714de0ee66f594074222c790b07c
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-04 18:41:02 +0000

    task 6

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+from calculator_1 import add, sub, mul, div
+from sys import argv
+
+if __name__ == "__main__":
+    a = int(argv[1])
+    b = int(argv[3])
+
+    if argv[2] == "+":
+        print("{} + {} = {}".format(a, b, add(a, b)))
+    elif argv[2] == "-":
+        print("{} - {} = {}".format(a, b, sub(a, b)))
+    elif argv[2] == "*":
+        print("{} * {} = {}".format(a, b, mul(a, b)))
+    elif argv[2] == "/":
+        print("{} / {} = {}".format(a, b, div(a, b)))
+    else:
+        print("Unknown operator. Available operators: +, -, * and /")
+        exit(1)
