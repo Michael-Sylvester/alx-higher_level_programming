5718cbd327e01ca879cab8c9ba6ff82b3d472f39
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-08 16:57:27 +0000

    task 10

---
+++ 0x03-python-data_structures/10-divisible_by_2.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def divisible_by_2(my_list=[]):
+    answer =[]
+    for x in my_list:
+        if my_list[x] % 2 == 0:
+            answer.append(True)
+        else:
+            answer.append(False)
+    return answer
