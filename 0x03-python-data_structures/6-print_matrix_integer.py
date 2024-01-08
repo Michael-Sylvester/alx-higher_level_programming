29409685dcb2617b3912870f554fdbf4cce5a707
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-08 16:33:45 +0000

    task 7

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -4,5 +4,5 @@ def print_matrix_integer(matrix=[[]]):
         for e in range(len(mem)):
             print("{:d}".format(mem[e]), end="")
             if e != len(mem) - 1:
-                print("",end=" ")
+                print("", end=" ")
         print("")
---
+++ 0x03-python-data_structures/7-add_tuple.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def add_tuple(tuple_a=(), tuple_b=()):
+    list = [0, 0, 0, 0]
+    count = 0
+    for x in tuple_a:
+        if count < 2:
+            list[count] = x
+            count += 1
+        else:
+            break
+
+    for x in tuple_b:
+        if count < 4:
+            list[count] = x
+            count += 1
+        else:
+            break;
+
+    sum1 =list[0] + list[2]
+    sum2 = list[1] +list[3]
+    return (sum1, sum2)
