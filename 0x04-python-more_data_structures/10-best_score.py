6bea7bd247733c5f5464f41c416d3185d6475331
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-10 17:21:39 +0000

    task 9 and 10

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    dic = a_dictionary
+    bestvalue = 0
+    best = ""
+    
+    if dic == None or len(dic) == 0:
+        return None
+    elif len(dic) == 1:
+        best = list(dic)[0]
+    else:
+        allkeys = list(dic)
+        best = allkeys[0]
+        bestvalue = dic[best]
+        for key in allkeys:
+            if dic[key] > bestvalue:
+                bestvalue = dic[key]
+                best = key
+    return best
