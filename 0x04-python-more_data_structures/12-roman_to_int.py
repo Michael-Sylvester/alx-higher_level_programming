975eb9d4e4b6f28d2934c20b53d00b5025e40729
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-10 18:07:57 +0000

    task 12 update

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -6,5 +6,7 @@ def roman_to_int(roman_string):
     sum = 0
     roman = dict(I=1, V=5, X=10, L=50, C=100, D=500, M=1000)
     for letter in roman_string:
-        sum += roman[letter]
+        sum += roman.get(letter, 0)
     return sum
+#roman_number = "MVTII"
+#print("{} = {}".format(roman_number, roman_to_int(roman_number)))
