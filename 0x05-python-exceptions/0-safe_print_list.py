001932b4cc512e1708aab0b7f9bd103bad6647d0
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-22 14:02:10 +0000

    task 0

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    index  = 0
+    try:
+        while (index < x):
+            print("{}".format(my_list[index]), end='')
+            count += 1
+            index += 1
+    except IndexError:
+        pass
+    finally:
+        print("")
+    return count
+
+my_list = [1, 2, 3, 4, 5]
+
+nb_print = safe_print_list(my_list, 2)
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list))
+print("nb_print: {:d}".format(nb_print))
+nb_print = safe_print_list(my_list, len(my_list) + 2)
+print("nb_print: {:d}".format(nb_print))
+        
