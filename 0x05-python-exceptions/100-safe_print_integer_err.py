cb907094953c0e4def5729483b8714887bac6e44
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-22 15:40:12 +0000

    advanced task 7

---
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+def safe_print_integer_err(value):
+    import sys
+    try:
+        print("{:d}".format(value))
+    except ValueError as verr:
+        print("Exception: {}".format(verr), file=sys.stderr)
+        return False
+    else:
+        return True
