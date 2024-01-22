6956f27081617e8a4915a2fd35aebd412e508ac7
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-22 15:42:55 +0000

    advanced task 7 update

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -6,5 +6,8 @@ def safe_print_integer_err(value):
     except ValueError as verr:
         print("Exception: {}".format(verr), file=sys.stderr)
         return False
+    except TypeError:
+        print("Exception: {}".format(verr), file=sys.stderr)
+        return False
     else:
         return True
