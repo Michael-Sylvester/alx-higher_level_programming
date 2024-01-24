4305884cfb98e677f382b6bdb4edc74970ad6bab
Michael Sylvester <42616131+Michael-Sylvester@users.noreply.github.com>
2024-01-24 10:47:02 +0000

    task 2 update

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -5,13 +5,7 @@
 class Square:
     """ Initialise empty square class"""
     def __init__(self, size=0):
-        """ Initialize size 
-        Args:
-            size (int, optional): The side length of the square. Defaults to 0.
-
-        Raises:
-            ValueError: If the side length is negative.
-            TypeError: If the side length is not an integer."""
+        """ Initialize size """
         if not isinstance(size, int):
             raise TypeError("size must be an integer")
         if size < 0:
