f303658626dffc8191541a1102560c8d2cc3b10f
Michael Sylvester <42616131+Michael-Sylvester@users.noreply.github.com>
2024-01-24 10:50:30 +0000

    task 3

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+"""Code that does something with squares"""
+
+
+class Square:
+    """ Initialise empty square class"""
+    def __init__(self, size=0):
+        """ Initialize size """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = size
+    
+    def area(self):
+        return self.__size ** 2
