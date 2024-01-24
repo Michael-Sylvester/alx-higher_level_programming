eae8e619379eb35b6241075e39d6b55affcebd15
Michael Sylvester <42616131+Michael-Sylvester@users.noreply.github.com>
2024-01-24 09:41:12 +0000

    task 1 update

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
-"""Code that does something """
+"""Code that does something with squares"""
 
 
 class Square:
     """ Initialise empty square class"""
     def __init__(self, size):
-        """Initialize Non to a vlaue """
-        if size is not None:
-            self.__size = size
+        """Initialize size """
+        self.__size = size
