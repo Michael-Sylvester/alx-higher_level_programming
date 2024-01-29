5b32a288afc51efc303c3e99dd253b70342edd13
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-29 22:26:44 +0000

    task 5

---
+++ 0x08-python-more_classes/5-rectangle.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python3
+""" Module for work on More_Python_Classes"""
+
+
+class Rectangle:
+    """ Class for rectangle reaction"""
+
+    def __init__(self, width=0, height=0):
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
+
+    def area(self):
+        """Calculates the area of the rectangle"""
+        return self.__height * self.__width
+
+    def perimeter(self):
+        """calculates the perimiter of the rectangle"""
+        perim = (2 * self.__height) + (2 * self.__width)
+
+        if self.__width == 0 or self.__height == 0:
+            perim = 0
+        return perim
+
+    def __str__(self):
+        """ String representation of the rectangle for printing. """
+
+        value = ""
+        if self.__width == 0 or self.__height == 0:
+            return value
+        else:
+            for h in range(self.__height):
+                value += ("#" * self.__width)
+                if h < self.__height - 1:
+                    value += "\n"
+        return value
+
+    def __repr__(self):
+        """Returns the official representation of the class instance"""
+        rep = ("Rectangle({}, {})".format(self.__width, self.__height))
+        return rep
+
+    def __del__(self):
+        print("Bye rectangle...")
