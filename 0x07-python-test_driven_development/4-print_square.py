ae8d45a4b750c58c179f27bec5e3868b36c89683
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-29 13:40:06 +0000

    task 2 and 3: say my name and print square

---
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -0,0 +1,14 @@
+""" module for printing squares """
+
+
+def print_square(size):
+    """ function for printing a square ou of # """
+    if isinstance(size, float) and size < 0:
+        raise TypeError("size must be an integer")
+    if not isinstance(size, int):
+        raise TypeError("size must be an integer")
+    if size < 0:
+        raise ValueError("size must be >= 0")
+
+    for no in range(size):
+        print("#" * size)
