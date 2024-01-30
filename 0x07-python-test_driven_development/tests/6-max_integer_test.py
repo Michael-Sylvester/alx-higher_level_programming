0e26bc18575082e0965f35a5823fb5330d2152d7
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-30 03:31:47 +0000

    task 5: max inteer

---
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""Python modual for add function"""
+
+
+import unittest
+max_integer = __import__('6-max_integer').max_integer
+
+class TestMaxInteger(unittest.TestCase):
+    """Class holding unit tests for max integer function"""
+    
+    def test_maxInteger(self):
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([2, 5, 9, 100]), 100)
+        self.assertAlmostEqual(max_integer([-2, -5, -9, -100]), -2)
+        self.assertAlmostEqual(max_integer([-2, -5, 2, -100]), 2)
+        self.assertAlmostEqual(max_integer([]), None)
+        self.assertAlmostEqual(max_integer([5]), 5)
+        self.assertAlmostEqual(max_integer([-2, 100, 9, 100]), 100)
+        
+    def test_values(self):
+        pass
+
+    def test_inputTypes(self):
+        self.assertRaises(TypeError, max_integer, "gg")
+        self.assertRaises(TypeError, max_integer, [[4, 5, 6], [55, 33, 98]])
+        self.assertRaises(TypeError, max_integer, [{2, 4}, {55, 77}])
+        self.assertRaises(TypeError, max_integer, ["33", "44", "98"])
+        self.assertRaises(TypeError, max_integer, [1.1, 3.55, 55.5])
