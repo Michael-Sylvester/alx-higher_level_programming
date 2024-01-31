f2c9d78a9397fa3108cd4e470a665d92bd28a615
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-01-31 03:06:19 +0000

    advanced task 6: not finished

---
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""Module for matrix multiplication"""
+
+
+def matrix_mul(m_a, m_b):
+    """Function that multiplies 2 matrixes agaist themselves"""
+    len(m_a[0]) == len(m_b)
