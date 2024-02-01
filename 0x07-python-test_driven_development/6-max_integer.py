46e12bc47c98e8c1f0722ef0d33a14b01593f5cb
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-01 12:25:48 +0000

    more unit tests

--- 0x07-python-test_driven_development/6-max_integer.py
+++ 0x07-python-test_driven_development/6-max_integer.py
@@ -1,26 +1,18 @@
 #!/usr/bin/python3
-"""Module to find the max integer in a a_list
+"""Module to find the max integer in a list
 """
 
 
-def max_integer(a_list=[]):
-    """Function to find and return the max integer in a a_list of integers
-        If the a_list is empty, the function returns None
+def max_integer(list=[]):
+    """Function to find and return the max integer in a list of integers
+        If the list is empty, the function returns None
     """
-    if not isinstance(a_list, list):
-        raise TypeError("a_list must be a list")
-    
-    if len(a_list) == 0:
+    if len(list) == 0:
         return None
-    
-    for mem in a_list:
-        if not isinstance(mem, int):
-            raise TypeError("a_List must contain integers")
-    
-    result = a_list[0]
+    result = list[0]
     i = 1
-    while i < len(a_list):
-        if a_list[i] > result:
-            result = a_list[i]
+    while i < len(list):
+        if list[i] > result:
+            result = list[i]
         i += 1
     return result
--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -10,20 +10,68 @@ class TestMaxInteger(unittest.TestCase):
     """Class holding unit tests for max integer function"""
 
     def test_positive_numbers(self):
-        self.assertAlmostEqual(max_integer([]), None)
-        self.assertAlmostEqual(max_integer([2, 5, 9, 100]), 100)
-        self.assertAlmostEqual(max_integer([5]), 5)
+        self.assertEqual(max_integer([]), None)
+        self.assertEqual(max_integer([2, 5, 9, 100]), 100)
+
+    def test_one_number(self):
+        self.assertEqual(max_integer([5]), 5)
+
+    def test_multiple_max_number(self):
+        self.assertEqual(max_integer([5, 2, 5, 3]), 5)
+
+    def test_very_large_number(self):
+        self.assertAlmostEqual(max_integer([50000000000000000000000, 2, 5, 3]), 50000000000000000000000)
+
+    def test_long_list(self):
+        self.assertEqual(max_integer([5, 2, 56, 3, 543, 3, 345, 34, 76, 765, 243, 23, 23, 54, 6, 5, 63]), 765)
 
     def test_negetive_numbers(self):
-        self.assertAlmostEqual(max_integer([-2, -5, -9, -100]), -2)
-        self.assertAlmostEqual(max_integer([-2, -5, 2, -100]), 2)
+        self.assertEqual(max_integer([-2, -5, -9, -100]), -2)
+        self.assertEqual(max_integer([-2, -5, 2, -100]), 2)
 
     def test_emptylist(self):
-        self.assertAlmostEqual(max_integer([]), None)
+        self.assertEqual(max_integer([]), None)
+    
+    def test_no_argument(self):
+        self.assertEqual(max_integer(), None)
+
+    def test_int_and_floats(self):
+        self.assertAlmostEqual(max_integer([1.1, 3, 55.5, 49]), 55.5)
+
+    def test_small_floats(self):
+        self.assertAlmostEqual(max_integer([0.44, 0.0004, 0.234, 0.465, -0.3534535, 0.8]), 0.8)
+
+    def test_list_of_string_numbers(self):
+        self.assertEqual(max_integer(["192834754"]), "192834754")
+
+    def test_list_of_string_int_list(self):
+        self.assertEqual(max_integer([["1"], ["9"], ["2"], ["8"]]), ["9"])
+        self.assertEqual(max_integer(["33", "44", "98"]), "98")
+
+    def test_list_of_string_list(self):
+        self.assertEqual(
+            max_integer([["hii"], ["ggs"], ["bbc"], ["sic"], ["ric"]]), ["sic"])
+
+    def test_lists_of_lists_ofInt(self):
+        self.assertEqual(max_integer([[4, 5, 6], [55, 33, 98]]), [55, 33, 98])
+
+    def test_infinity(self):
+        self.assertEqual(max_integer([464, float('inf'), float('-inf')]), float('inf'))
+
+    def test_nan(self):
+        self.assertEqual(max_integer([543, float('nan'), 433654]), 433654)
 
     def test_inputTypes(self):
-        self.assertRaises(TypeError, max_integer, "gg")
-        self.assertRaises(TypeError, max_integer, [[4, 5, 6], [55, 33, 98]])
         self.assertRaises(TypeError, max_integer, [{2, 4}, {55, 77}])
-        self.assertRaises(TypeError, max_integer, ["33", "44", "98"])
-        self.assertRaises(TypeError, max_integer, [1.1, 3.55, 55.5])
+
+    def test_mixed_list(self):
+        self.assertRaises(TypeError, max_integer,[44, 56, 756,"34"])
+
+    def test_none(self):
+        self.assertRaises(TypeError, max_integer, None)
+
+    def test_dictionary(self):
+        self.assertRaises(TypeError, max_integer, [{20: 23, 14: 45}, {"a": "b"}])
+
+    def test_int(self):
+        self.assertRaises(TypeError, max_integer, 33)
