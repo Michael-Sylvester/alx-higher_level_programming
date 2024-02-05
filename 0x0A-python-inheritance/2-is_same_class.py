c6e5350b7d362b17ad5d0055263514ba341c8d3d
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-05 21:19:09 +0000

    task 2: docstring  added

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,7 +3,8 @@
 
 
 def is_same_class(obj, a_class):
-    if type(obj)  == a_class:
+    """function to check objects against a class"""
+    if type(obj) is a_class:
         return True
     else:
         return False
