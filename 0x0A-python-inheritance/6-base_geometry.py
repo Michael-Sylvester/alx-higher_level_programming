8f6df759fac91a712436d52426e5194267b7023d
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-05 22:36:24 +0000

    task 5: def area in class geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""module for base BaseGeometry"""
+
+
+class BaseGeometry:
+    """base class for further tasks"""
+    def area(self):
+        raise Exception("area() is not implemented")
