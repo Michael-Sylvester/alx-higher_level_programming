5ff452985149459046614bbdacfc357e358f1eb2
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-05 21:31:25 +0000

    task 4: check obj agaist class to check for inheritance

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,7 @@
 
 def is_kind_of_class(obj, a_class):
     """function to check objects against a class and its children"""
-    if isinstance(obj, a_class):
+    if issubclass(obj, a_class):
         return True
     else:
         return False
