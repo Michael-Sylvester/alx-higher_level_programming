e7c62d74ad5db8e9b9e1815e819e6ce52821a57c
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-05 22:07:05 +0000

    task 4: update code

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,8 +4,12 @@
 
 def inherits_from(obj, a_class):
     """function to check objects against a class and its children"""
-    if isinstance(obj, a_class):
-        return True
+    allclasses = type(obj).__mro__
+    for cls in allclasses:
+        if cls is a_class:
+            return False
+        if issubclass(cls, a_class):
+            return True
     else:
         return False
 if __name__ == "__main__":
