993ff8f5e50f653e830e48656231812790c6f0f2
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-05 21:42:53 +0000

    task 4: wrong function name corrected

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -2,7 +2,7 @@
 """module for checking obj class"""
 
 
-def is_kind_of_class(obj, a_class):
+def inherits_from(obj, a_class):
     """function to check objects against a class and its children"""
     if issubclass(obj, a_class):
         return True
