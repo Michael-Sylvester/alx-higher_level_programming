965cae8f77bbe5624ceb003e36d59c64b07ba6b5
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-05 21:44:51 +0000

    task 4: must use type(obj)

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -4,7 +4,9 @@
 
 def inherits_from(obj, a_class):
     """function to check objects against a class and its children"""
-    if issubclass(obj, a_class):
+    if issubclass(type(obj), a_class):
         return True
     else:
         return False
+if __name__ == "__main__":
+    pass
