55693b49ae16d6e8e77f2224baa9633586a0ba3d
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-06 21:32:34 +0000

    task 6: retrieve json data from file and convert it

--- 0x0B-python-input_output/5-save_to_json_file.py
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -7,7 +7,6 @@ def save_to_json_file(my_obj, filename):
     """File to write text to a file"""
     with open(filename, 'w', encoding="utf-8") as json_file:
         json.dump(my_obj, json_file)
-   
 
 
 if __name__ == "__main__":
---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""Module for appending from files"""
+import json
+
+
+def load_from_json_file(filename):
+    """Function for converting file data from json format to object"""
+    with open(filename, 'r', encoding="utf-8") as file:
+        data = json.load(file)
+    return data
