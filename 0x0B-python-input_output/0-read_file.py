7fc1483c52ed820e8ccb0117dc6936dcae9cc9c9
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-06 19:54:13 +0000

    task 0 update: pycode style

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -6,7 +6,7 @@ def read_file(filename=""):
     """File to read all info from a file"""
     with open(filename, 'r', encoding='utf-8') as file:
         print(file.read(), end="")
-   
+
 
 if __name__ == "__main__":
     pass
