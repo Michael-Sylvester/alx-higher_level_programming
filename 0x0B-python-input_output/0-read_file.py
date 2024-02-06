1078f5f71173e8d9148385f12edd1537c8cde673
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-06 19:47:37 +0000

    task 0 update: encoding utf-8

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -4,10 +4,10 @@
 
 def read_file(filename=""):
     """File to read all info from a file"""
-    with open(filename, 'r') as file:
+    with open(filename, 'r', encoding='utf-8') as file:
         line = file.read()
     print(line)
-
+   
 
 if __name__ == "__main__":
     pass
