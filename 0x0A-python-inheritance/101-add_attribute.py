fd24c285d77d78c013487466535c7dabefabaaf0
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-06 01:49:17 +0000

    task 13: safe setattr() function

---
+++ 0x0A-python-inheritance/101-add_attribute.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""Module for add_atrtibute Function"""
+
+
+def add_attribute(obj, attr, value):
+    """Checks if valid stribut is being set"""
+    if not isinstance(attr, str):
+        raise TypeError("can't add new attribute")
+
+    if not isinstance(obj, object):
+        raise TypeError("can't add new attribute")
+
+    if hasattr(obj, '__slots__') and attr not in obj.__slots__:
+        raise TypeError("can't add new attribute")
+
+    builtin_types = [int, str, list, dict, tuple, set,co
+                     float, bool, complex, bytes]
+    if type(obj) in builtin_types:
+        raise TypeError("can't add new attribute")
+
+    status = setattr(obj, attr, value)
