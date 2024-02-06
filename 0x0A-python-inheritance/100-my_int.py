d436d221c6948a1d3a5a3fbf05f4d64ad9ee2bf5
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-06 01:04:30 +0000

    task 12: custom int class

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -3,8 +3,11 @@
 
 
 class MyInt(int):
+    """CLass for custom int Class"""
     def __eq__(self, __value: object) -> bool:
+        """Runs the not equal to when called"""
         return super().__ne__(__value)
-    
+
     def __ne__(self, __value: object) -> bool:
+        """Runs the equal to built-in when called"""
         return super().__eq__(__value)
