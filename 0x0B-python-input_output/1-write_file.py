8d9f04465c4c096eb75cb4788ae5a876eeb1cdc0
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-06 20:21:35 +0000

    task 1: read file

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Module for reading from files"""
+
+
+def write_file(filename="", text=""):
+    """File to write text to a file"""
+    with open(filename, 'w', encoding='utf-8') as file:
+        num = file.write(text)
+    return num
+
+
+if __name__ == "__main__":
+    pass
