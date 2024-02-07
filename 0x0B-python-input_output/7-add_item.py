1c0f3409d1c272b5fcb32bcfa7c0daa0d252a0dc
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-07 00:34:12 +0000

    task 7: newline function not needed

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -7,18 +7,11 @@ load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
 save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
 
 
-def addnewline(filename):
-    """adds new line to end of json file"""
-    with open(filename, 'a', encoding="utf-8") as f:
-        f.write('\n')
-
-
 filename = "add_item.json"
 
 if not os.path.exists(filename):
     newlist = list()
     save_to_json_file(newlist, filename)
-    addnewline(filename)
     print("New {} file created".format(filename))
 else:
     print("opening {}".format(filename))
@@ -28,4 +21,3 @@ else:
         for i in range(1, len(sys.argv)):
             filelist.append(sys.argv[i])
         save_to_json_file(filelist, filename)
-        addnewline(filename)
