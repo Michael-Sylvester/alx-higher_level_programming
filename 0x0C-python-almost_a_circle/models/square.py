99c2e26cf4e10db52091a9096a8a14f1c721016a
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-02-12 21:31:20 +0000

    pydoc in sqaure class

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -4,6 +4,7 @@ from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
+    """CLass sqaure is a child of rectangle"""
     def __init__(self, size, x=0, y=0, id=None):
         super().__init__(size, size, x, y, id)
 
