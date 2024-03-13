ce12a2bbe9d24b78bdd5c12e4fcf50963615ea79
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-13 04:04:32 +0000

    all advanced tasks

---
+++ 0x0D-SQL_introduction/101-avg_temperatures.sql
@@ -0,0 +1,5 @@
+-- Group data by avg
+SELECT city, AVG(value) AS avg_temp
+FROM temperatures
+GROUP BY city
+ORDER BY avg_temp DESC;
---
+++ 0x0D-SQL_introduction/103-max_state.sql
@@ -0,0 +1,5 @@
+-- Group data by max
+SELECT state, MAX(value) AS max_temp
+FROM temperatures
+GROUP BY state
+ORDER BY state;
