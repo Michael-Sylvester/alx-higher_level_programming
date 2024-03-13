cf66f0497145a57bfc96ce3a52a9e5442d076896
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-13 20:57:42 +0000

    task 6 and 7

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,9 @@
+-- create database and table
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+
+USE hbtn_0d_usa;
+
+CREATE TABLE IF NOT EXISTS states(
+    id INT NOT NULL DEFAULT 1 AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
