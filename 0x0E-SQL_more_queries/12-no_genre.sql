7b5358f44bf9b67ed23bf1e986bdfab18bc1ba24
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-13 23:14:54 +0000

    task 10, 11 and 12

--- 0x0E-SQL_more_queries/12-no_genre.sql
+++ 0x0E-SQL_more_queries/12-no_genre.sql
@@ -2,6 +2,6 @@
 
 SELECT tv_shows.title, IFNULL(tv_show_genres.genre_id, 'NULL') AS genre_id
 FROM tv_shows
-left JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id 
+LEFT JOIN tv_show_genres ON tv_show_genres.show_id = tv_shows.id 
 WHERE tv_show_genres.genre_id IS NULL 
 ORDER BY tv_shows.title, tv_show_genres.genre_id;
