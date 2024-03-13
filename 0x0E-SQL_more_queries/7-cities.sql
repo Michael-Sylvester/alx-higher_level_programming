80f81cd90b4f436bf1d65d14be4002613e9ad2f2
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-13 21:12:56 +0000

    task 6and 7, default not needed when auto_increment is added

--- 0x0E-SQL_more_queries/7-cities.sql
+++ 0x0E-SQL_more_queries/7-cities.sql
@@ -1,10 +1,11 @@
--- Creates dtabase and table
+-- Creates database and table
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
 
 USE hbtn_0d_usa;
 
 CREATE TABLE IF NOT EXISTS cities(
-    id INT NOT NULL DEFAULT 1 AUTO_INCREMENT PRIMARY KEY,
-    state_id INT NOT NULL FOREIGN KEY,
-    name VARCHAR(256) NOT NULL
+    id INT  AUTO_INCREMENT PRIMARY KEY,
+    state_id INT NOT NULL,
+    name VARCHAR(256) NOT NULL,
+    FOREIGN KEY (state_id) REFERENCES states(id)
 );
