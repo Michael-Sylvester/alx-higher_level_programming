75f00463d8c443b45e299221105bb88a1803fe49
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-13 18:28:58 +0000

    initialization and task0

--- 0x0E-SQL_more_queries/0-privileges.sql
+++ 0x0E-SQL_more_queries/0-privileges.sql
@@ -1,3 +1,3 @@
 -- display all privilages
-SHOW GRANTS OF 'user_0d_1'@'localhost';
-SHOW GRANTS OF 'user_0d_2'@'localhost';
+SHOW GRANTS FOR 'user_0d_1'@'localhost';
+SHOW GRANTS FOR 'user_0d_2'@'localhost';
