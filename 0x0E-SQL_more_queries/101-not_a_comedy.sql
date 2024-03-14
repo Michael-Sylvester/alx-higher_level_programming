18a96f559de3e37d4925a805d3dcc29413573ce6
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-14 01:57:52 +0000

    advanced task 18

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,11 @@
+-- All shows not of a genre
+
+SELECT title
+FROM tv_shows
+WHERE id NOT IN(
+    SELECT show_id
+    FROM tv_show_genres
+    JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+    WHERE tv_genres.name = 'Comedy'
+)
+ORDER BY title;
