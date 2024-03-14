b9e9ddc4c218298ef43236e1dc84e509f16f4e2e
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-14 01:48:22 +0000

    advanced task 17

---
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -0,0 +1,11 @@
+-- All genres not of 1 show
+
+SELECT name
+FROM tv_genres
+WHERE id NOT IN (
+    SELECT genre_id
+    FROM tv_show_genres
+    JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+)
+ORDER BY name ASC;
