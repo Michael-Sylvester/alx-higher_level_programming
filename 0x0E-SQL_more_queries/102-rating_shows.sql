d2024059cb07a0a00a1f6f1718be51d761d3b813
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-14 02:16:52 +0000

    advanced task19

--- 0x0E-SQL_more_queries/102-rating_shows.sql
+++ 0x0E-SQL_more_queries/102-rating_shows.sql
@@ -1,6 +1,6 @@
 -- List all shows with rating
 
-SELECT title, SUM(rate) as rating
+SELECT title, SUM(rate) AS rating
 FROM tv_shows, tv_show_ratings
 WHERE tv_shows.id = tv_show_ratings.show_id
 GROUP BY title
