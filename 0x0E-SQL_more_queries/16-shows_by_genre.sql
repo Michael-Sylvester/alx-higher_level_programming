533a40f8fbd55dd120c905f9bce63cfad4b4df2b
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-03-14 01:13:45 +0000

    task 15 and 16

---
+++ 0x0E-SQL_more_queries/16-shows_by_genre.sql
@@ -0,0 +1,7 @@
+-- everyshow and the genres linked to them
+
+SELECT tv_shows.title as title, COALESCE(tv_genres.name, 'NULL') AS name
+FROM tv_shows
+LEFT JOIN tv_show_genres ON tv_shows.id = tv_show_genres.show_id
+LEFT JOIN tv_genres ON tv_genres.id = tv_show_genres.genre_id
+ORDER BY tv_shows.title, tv_genres.name;
