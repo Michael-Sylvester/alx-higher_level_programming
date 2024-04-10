1708cf4d44220152495af892330eb80313dcb1b5
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-04-10 18:10:51 +0100

    advanced task 100 updated to use mmap method

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
-const list = require('./101-data.js').list;
+const list = require('./100-data.js').list;
 const listB = list.map((num, index) => num * index);
 
 console.log(list);
