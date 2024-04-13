bf989f5d1d9b23c6351b855b3b906d3f0e203bdc
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-04-13 03:55:23 +0100

    task 7

---
+++ 0x0F-python-object_relational_mapping/7-model_state_fetch_all.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+"""list all State objects from the database
+"""
+import sys
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import sessionmaker
+
+Base = declarative_base()
+
+
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format
+                           (sys.argv[1], sys.argv[2], sys.argv[3]))
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    states = session.query(State).order_by(State.id.asc())
+    for state in states:
+        print("{}: {}".format(state.id, state.name))
--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""Start link class to table in database 
+"""Start link class to table in database
 """
 import sys
 from sqlalchemy import create_engine
@@ -9,17 +9,19 @@ from sqlalchemy import Column, Integer, String
 # Define Base instance
 Base = declarative_base()
 
+
 class State(Base):
     """State class"""
     __tablename__ = 'states'
     id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
     name = Column(String(128), nullable=False)
 
+
 if __name__ == "__main__":
     # Connect to MySQL server
     engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
-                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), 
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
                            pool_pre_ping=True)
-    
+
     # Create all tables in the engine
     Base.metadata.create_all(engine)
