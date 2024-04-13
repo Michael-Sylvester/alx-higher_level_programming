398d8c95fbfa5374e266fe0e443691fa85c97b00
Michael-Sylvester <mike.k.sylvester@gmail.com>
2024-04-13 01:59:08 +0100

    adcheck task 0

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
+"""Module to query a table for all rows"""
 import MySQLdb
 import sys
-"""Module to query a table for all rows"""
 
-db = MySQLdb.connect(host="localhost", port="3306", user=sys.argv[1],
-                     passwd=sys.argv[2], db=sys.argv[3])
-cur = db.cursor()
 
-cur.execute("SELECT * FROM states ORDER BY id")
-
-rows = cur.fetchall()
-for row in rows:
-    print("%s".format(row))
-cur.close()
 if (__name__ == "__main__"):
-    pass
+        
+    db = MySQLdb.connect(host="localhost", port="3306", user=sys.argv[1],
+                        passwd=sys.argv[2], db=sys.argv[3])
+    cur = db.cursor()
+
+    cur.execute("SELECT * FROM states ORDER BY id")
+
+    rows = cur.fetchall()
+    for row in rows:
+        print("%s".format(row))
+    cur.close()
